7c7f764c47af3c019188a0a51aee66dd38017b1b
ChrisCoder9000 <alch.infoemail@gmail.com>
2022-10-14 11:03:19 +0200

    Initial Commit

---
+++ ChatSpot/ChatSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatSpotApp.swift
+//  ChatSpot
+//
+//  Created by Christian Nonis on 14/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
